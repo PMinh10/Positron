1039cbdff201eb2a75f925f12028f8ceb60156cf
PMinh10 <dinhphuminh2007@gmail.com>
2025-03-07 14:13:30 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+function login()
+{
+  let u=un.value;
+  let p=pass.value;
+  if(u=="minh" && p=="1")
+  {
+    window.open("home.html");
+    document.getElementById(compiler).style.display="block";
+  }
+}
